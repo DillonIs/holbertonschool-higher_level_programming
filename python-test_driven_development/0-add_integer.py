9ecddd3960e914758a95a1950fa4a39269365f29
DillonIs <dillonis826@gmail.com>
2025-05-18 09:29:29 +1000

    Fixed function file to work in the coding style

--- python-test_driven_development/0-add_integer.py
+++ python-test_driven_development/0-add_integer.py
@@ -8,25 +8,25 @@ Defines a function as add_integer(a, b=98) which is used to add a and b.
 
 def add_integer(a, b=98):
     """ Python function to add two integers and or floats as defined by a and b
-    
+
     Arguments:
         a: first value
         b: second value
-        
+
     Returns:
         The result of adding the two values
-    
+
     Raises:
         TypeError: If a or b aren't integer and/or float
-        
+
     """
 
     if not isinstance(a, int) and not isinstance(a, float):
         raise TypeError("a must be an integer")
-    
+
     if not isinstance(b, int) and not isinstance(b, float):
         raise TypeError("b must be an integer")
-    
+
     if isinstance(a, float):
         a = int(a)
 
