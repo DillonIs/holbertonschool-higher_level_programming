a1361d82fed3c78951e12090a8dd0649b4105c30
DillonIs <dillonis826@gmail.com>
2025-05-19 09:32:36 +1000

    Python function that prints names with doc test file

--- python-test_driven_development/3-say_my_name.py
+++ python-test_driven_development/3-say_my_name.py
@@ -1,2 +1,19 @@
 #!/usr/bin/python3
+"""Function for printing a name
+Prints a name
+"""
+
+
 def say_my_name(first_name, last_name=""):
+    """Variables:
+    first_name(str): First name
+    last_name(str): Last name
+    
+    Raises:
+        TypeError: If either name is not a string
+    """
+    if not isinstance(first_name, str):
+        raise TypeError("first_name must be a string")
+    if not isinstance(last_name, str):
+        raise TypeError("last_name must be a string")
+    print("My name is {} {}".format(first_name, last_name))
