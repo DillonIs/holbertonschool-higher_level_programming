8f0b0e1dd00a7a9f536171ecd62b6dd4d4f8ab52
DillonIs <dillonis826@gmail.com>
2025-05-20 10:57:38 +1000

    Class that returns the square area of Square with a public instance

---
+++ python-classes/3-square.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+"""Define a class as Square based on 2-square.py"""
+
+
+class Square:
+    """Define the properties of Square
+    Instances:
+    size - Private instance
+    area - Public instance
+    size must be a positive integer otherwise raise errors
+    """
+    def __init__(self, size=0):
+        """Private instance of Square"""
+        self.__size = size
+        if not isinstance(size, int):
+            raise TypeError("size must be an integer")
+        elif size < 0:
+            raise ValueError("size must be >= 0")
+        else:
+            self.__size = size
+    def area(self):
+        """Finds the area of Square
+        Returns: The current square area
+        """
+        return self.__size ** 2
