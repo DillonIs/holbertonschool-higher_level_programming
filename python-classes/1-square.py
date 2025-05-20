1cf3b1645a3629e411c9ac4968ad26f397e46b97
DillonIs <dillonis826@gmail.com>
2025-05-20 10:29:17 +1000

    Class that defines a square with instance attributes

---
+++ python-classes/1-square.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""Define class as Square"""
+
+
+class Square:
+    """
+    Define the properties of square based on 0-square.py
+
+    Attributes:
+        size: Defines the size of Square
+    """
+    def __init__(self, size):
+        """Private instance of Square as size
+        Defines the size of Square"""
+        self.__size = size
