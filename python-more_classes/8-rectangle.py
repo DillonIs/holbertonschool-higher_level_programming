833e4e4f6f32bc730e00dda1f75ee89f4d2b813e
DillonIs <dillonis826@gmail.com>
2025-05-24 14:06:18 +1000

    Compare two rectangles with static method

--- python-more_classes/8-rectangle.py
+++ python-more_classes/8-rectangle.py
@@ -102,10 +102,7 @@ class Rectangle:
             raise TypeError("rect_1 must be an instance of Rectangle")
         if not isinstance(rect_2, Rectangle):
             raise TypeError("rect_2 must be an instance of Rectangle")
-
-        area_1 = rect_1.area()
-        area_2 = rect_2.area()
-        if area_1 >= area_2:
-            return area_1
+        if rect_1.area() >= rect_2.area():
+            return rect_1
         else:
-            return area_2
+            return rect_2
