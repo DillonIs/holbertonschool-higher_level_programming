d1507f8a9486e442f0302c7d2e151d0fa4dd517e
DillonIs <dillonis826@gmail.com>
2025-05-28 11:49:37 +1000

    Created an Abstract class as Animal with Dog and Cat

--- python-abc/task_00_abc.py
+++ python-abc/task_00_abc.py
@@ -1,30 +1,25 @@
-#!/usr/bin/env python3
-"""Abstract Class defined as Animal
-Using the ABC package
-Has an abstract method defined as Sound
-"""
-
-
+#!/usr/bin/python3
 from abc import ABC, abstractmethod
 
 class Animal(ABC):
     """Defines class as Animal
     inherits from ABC"""
+
     @abstractmethod
     def sound(self):
         """Abstract method defines the sound Animals make"""
         pass
 
-    class Dog(Animal):
-        """Class defined as Dog
-        Inherits from Animal"""
-        def sound(self):
-            """Defines and returns the sound a dog makes"""
-            return "Bark"
+class Dog(Animal):
+    """Class defined as Dog
+    Inherits from Animal"""
+    def sound(self):
+        """Defines and returns the sound a dog makes"""
+        return "Bark"
 
-    class Cat(Animal):
-        """Class defined as Cat
-        Inherits from Animal"""
-        def sound(self):
-            """Defines and returns the sound a cat makes"""
-            return "Meow"
+class Cat(Animal):
+    """Class defined as Cat
+    Inherits from Animal"""
+    def sound(self):
+        """Defines and returns the sound a cat makes"""
+        return "Meow"
