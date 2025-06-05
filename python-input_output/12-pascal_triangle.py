8a4c55de86845ff195deed88b294367a5e55bd65
DillonIs <dillonis826@gmail.com>
2025-06-05 12:41:53 +1000

    Python code that prints Pascals triangle

---
+++ python-input_output/12-pascal_triangle.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+"""Pascal's Triangle"""
+
+
+def pascal_triangle(n):
+    if n <= 0:
+        return []
+
+    triangle = [[1]]
+    while len(triangle) is not n:
+        """coef is the coefficient
+        triangle is the 2D array"""
+        coef = [1]
+        for i in range(len(triangle[-1]) - 1):
+            coef.append(triangle[-1][i] + triangle[-1][i + 1])
+        coef.append(1)
+        triangle.append(coef)
+    return triangle
