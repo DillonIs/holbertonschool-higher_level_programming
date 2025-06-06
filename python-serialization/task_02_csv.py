9d0022597c0a4361735f894829a80e6952b22979
DillonIs <dillonis826@gmail.com>
2025-06-06 11:50:19 +1000

    Python code to convert csv file into a JSON file

--- python-serialization/task_02_csv.py
+++ python-serialization/task_02_csv.py
@@ -10,10 +10,21 @@ def convert_csv_to_json(csv_file):
         """Serializes and deserializes
         Returns True if serialisation and deserialization has occurred"""
         with open('data.csv', 'r', encoding='utf-8') as csv_file:
-            data = list(csv.DictReader(csv_file))
-        with open('data.json', 'w', encoding='utf-8') as jsonfile:
-            json.dump(data, jsonfile, indent=4)
+            csv_read = csv.DictReader(csv_file)
+            data = [row for row in csv_read]
+
+        with open('data.json', 'w', encoding='utf-8') as json_file:
+            json.dump(data, json_file, indent=4)
+
         return True
     except FileNotFoundError:
         """Returns False if the file doesn't exist"""
         return False
+
+if __name__ == "__main__":
+    csv_file = "data.csv"
+    success = convert_csv_to_json(csv_file)
+    if success:
+        print(f"Data from {csv_file} has been converted to data.json")
+    else:
+        print(f"Failed to convert data from {csv_file}")
