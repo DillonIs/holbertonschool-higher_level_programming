a1ad60adb049c1b1ee3eb786f5f2c44f1a79cb74
DillonIs <dillonis826@gmail.com>
2025-06-29 12:18:25 +1000

    List all shows and all genres linked to that show

---
+++ SQL_more_queries/16-shows_by_genre.sql
@@ -0,0 +1,8 @@
+-- List all shows and all genres linked to that show
+SELECT g.name, t.title
+FROM tv_shows AS t
+LEFT JOIN tv_show_genres AS m
+ON t.id = m.show_id
+LEFT JOIN tv_genres AS g
+ON g.id = m.genre_id
+ORDER BY title ASC, name ASC;
