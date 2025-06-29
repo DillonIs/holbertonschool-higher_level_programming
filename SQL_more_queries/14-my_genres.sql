9f5d2f4af65435199512e3b51c944591bc224f43
DillonIs <dillonis826@gmail.com>
2025-06-29 12:01:36 +1000

    List the genres of a specific show

---
+++ SQL_more_queries/14-my_genres.sql
@@ -0,0 +1,9 @@
+-- Lists all genres of a specified show
+SELECT name
+FROM tv_genres AS g
+INNER JOIN tv_show_genres AS m
+ON g.id = m.genre_id
+INNER JOIN tv_shows t
+ON m.show_id = t.id
+WHERE t.title = 'Dexter'
+ORDER BY name ASC;
