db00c54b4e29aff0d03a6d163274eb416a8a2f5f
DillonIs <dillonis826@gmail.com>
2025-06-30 11:24:51 +1000

    Python code to connect and list data from a database

--- python-object_relational_mapping/0-select_states.py
+++ python-object_relational_mapping/0-select_states.py
@@ -1,24 +1,24 @@
 #!/usr/bin/python3
 """Python script for selecting states from a MySQL database"""
-
+import MySQLdb
+import sys
 
 if __name__ == "__main__":
-    from sys import argv
-    import MySQLdb
-
+    # Connect to MySQL using commandline arguments
     db = MySQLdb.connect(
-        user=argv[1],
-        password=argv[2],
-        database=argv[3]
+        host="localhost",
+        port=3306,
+        user=sys.argv[1],
+        password=sys.argv[2],
+        database=sys.argv[3]
     )
+    # Create a cursor and execute a SQL query
     cursor = db.cursor()
 
-    cursor.execute('SELECT * FROM states')
+    cursor.execute('SELECT * FROM states ORDER BY states.id ASC')
 
     for state in cursor.fetchall():
         print(state)
 
-    if cursor:
-        cursor.close()
-    if db:
-        db.close()
+    cursor.close()
+    db.close()
