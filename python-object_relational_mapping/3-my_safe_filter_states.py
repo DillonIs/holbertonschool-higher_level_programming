f49de73d8223da16fa03199ae7f6d9b2e60bd1bf
DillonIs <dillonis826@gmail.com>
2025-07-01 10:25:58 +1000

    Protecting against SQL injections

---
+++ python-object_relational_mapping/3-my_safe_filter_states.py
@@ -0,0 +1,33 @@
+#!/usr/bin/python3
+"""Filter based on 2-my_filter_states but prevent
+SQL injections"""
+import MySQLdb
+import sys
+
+if __name__ == "__main__":
+    # Connect to MySQL database
+    db = MySQLdb.connect(
+        host="localhost",
+        port=3306,
+        user=sys.argv[1],
+        password=sys.argv[2],
+        database=sys.argv[3]
+    )
+    state_name = sys.argv[4]
+    # Create a cursor and execute a SQL query
+    cursor = db.cursor()
+
+    #Sanitize by using %s (for script) in place of each variable
+    cursor.execute("""
+                   SELECT * FROM states
+                   WHERE name = BINARY %
+                   ORDER BY id ASC
+                   """, (state_name,))
+
+    result = cursor.fetchall()
+
+    for state in result:
+        print(state)
+
+    cursor.close()
+    db.close()
