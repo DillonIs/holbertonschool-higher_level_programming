a1570d0d00454629de0c0e24b17500c080497800
DillonIs <dillonis826@gmail.com>
2025-07-23 12:33:21 +1000

    Print a square

--- javascript-warm_up/8-square.js
+++ javascript-warm_up/8-square.js
@@ -5,7 +5,7 @@ const size = process.argv[2];
 if (isNaN(size)) {
   console.log('Missing size');
 } else {
-  for (let i = 0; i < parseInt(size); i++){
+  for (let i = 0; i < parseInt(size); i++) {
     console.log('X'.repeat(parseInt(size)));
   }
 }
