3dbf86330a2d788192e312ccb3f9c924ce86f8ca
DillonIs <dillonis826@gmail.com>
2025-07-23 10:02:40 +1000

    Convert argv into integers

--- javascript-warm_up/5-to_integer.js
+++ javascript-warm_up/5-to_integer.js
@@ -1,4 +1,4 @@
 #!/usr/bin/node
 
 const number = Math.floor(Number(process.argv[2]));
-console.log(isNaN(number) ? 'Not a number' : 'My number: ', number);
+console.log(isNaN(number) ? 'Not a number' : 'My number: ' + number);
