da368f49eab5d96fc132695dbfebc83a1b9dfc26
DillonIs <dillonis826@gmail.com>
2025-07-23 14:45:29 +1000

    Fixed errors with 2nd largest int

--- javascript-warm_up/11-second_biggest.js
+++ javascript-warm_up/11-second_biggest.js
@@ -3,6 +3,8 @@
 if (process.argv.length <= 3) {
   console.log(0);
 } else {
-  const numbers = process.argv.sort();
-  console.log(numbers.reverse()[1]);
+  const numbers = process.argv.map(Number)
+  .slice(2, process.argv.length)
+  .sort((a, b) => a - b);
+  console.log(numbers[numbers.length - 2]);
 }
