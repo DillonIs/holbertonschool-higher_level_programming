cb8450296680433714c5ee65eb679e0fb24c56e5
DillonIs <dillonis826@gmail.com>
2025-07-23 12:16:52 +1000

    Looping through an array JavaScript

--- javascript-warm_up/6-multi_languages_loop.js
+++ javascript-warm_up/6-multi_languages_loop.js
@@ -1,6 +1,6 @@
 #!/usr/bin/node
 
-const stringVar = ['C is fun', 'Python is cool', 'Javascript is amazing'];
+const stringVar = ['C is fun', 'Python is cool', 'JavaScript is amazing'];
 
 let i = 0;
 
